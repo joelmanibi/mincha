92492fd241a878bf9c54a6890702901c492f492c
MANI Joël /GOS <NKTR5049@ocitnetad.ci>
2024-10-08 00:16:15 +0000

    refont

--- src/models/ville.model.js
+++ src/models/ville.model.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, Sequelize) => {
         autoIncrement: true,
         primaryKey: true
       },
-      vileName: {
+      villeName: {
         type: Sequelize.STRING,
       }
     }
