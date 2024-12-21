4fd5786aecf4ec0a05ba2e2e0176e0fb6c1298f9
MANI Joël /GOS <NKTR5049@ocitnetad.ci>
2024-12-21 08:17:37 +0000

    mise a jour middleware 3

--- src/routes/account/owner/create-owner.routes.js
+++ src/routes/account/owner/create-owner.routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   app.post(
     "/api/account/create-owner",
     [
-      verifySignUp.checkDuplicateUser,
+      verifySignUp.checkDuplicateAccount,
     ],
     controller.createOwner
   );
