a59a887f263c64c59f29d885c6410ddd4f814c52
MANI Joël /GOS <NKTR5049@ocitnetad.ci>
2025-01-09 17:05:02 +0000

    test

--- src/controllers/account/users/getall-user.controller.js
+++ src/controllers/account/users/getall-user.controller.js
@@ -4,6 +4,19 @@ const UserType = db.userType;
 const UserRole = db.userRole;
 
 exports.getAllUser = async (req,res) => {
+
+  const requesterIsAdmin = await User.findOne({
+    where: {
+      userId: req.userId,
+      userRoleID: 1,
+      userTypeID: 5
+    }
+  });
+  if (!requesterIsAdmin) {
+    return res.status(403).send({
+      message: "Vous n'etes pas authorisé a éffectuer cette requete, merci de contacter l'administrateur pour support",
+    });
+  };
   
     try {
       const user = await User.findAll({
