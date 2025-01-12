ae2df979ec10a386af6e0be7ccc242147b753cbd
MANI Joël /GOS <NKTR5049@ocitnetad.ci>
2025-01-12 11:56:57 +0000

    update

--- src/controllers/property/getall-property.controller.js
+++ src/controllers/property/getall-property.controller.js
@@ -4,6 +4,7 @@ const PropertyDocType = db.propertyDocType;
 const PropertyLevel = db.level;
 const Property = db.property;
 const User = db.user;
+const AccountType = db.accountType
 const Account = db.account
 const util = require('util');
 
